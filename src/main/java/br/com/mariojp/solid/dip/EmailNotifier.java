a38200e96a6ca3db344edebbdafd86f8315e01e3
Aquiles <aquiles2ws@gmail.com>
2025-09-03 22:36:11 -0300

    refactor: atualiza o construtor da classe para verificar qual será a implementação de envio

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,19 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
-
+	private final MailSender mailSender;
+	
+	public EmailNotifier() {
+        if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
+            this.mailSender = new NoopMailSender();
+        } else {
+            this.mailSender = new SmtpClient();
+        }
+    }
 	public void welcome(User user) {
 		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
 			// Estado inicial: ainda assim usa SMTP real (bug proposital)
 		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
+		
 	}
 }
